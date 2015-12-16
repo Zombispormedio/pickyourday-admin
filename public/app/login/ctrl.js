a1849e599438a99b8135ce17d7ce78eae189f670
Xavier Serrano <zombispormedio007@gmail.com>
2015-12-16 20:33:41 +0100

    f

--- public/app/login/ctrl.js
+++ public/app/login/ctrl.js
@@ -3,7 +3,7 @@ adminCtrl.LoginCtrl = function ($rootScope, $scope, $http) {
 	$scope.user = {};
 	$scope.login = function () {
 		if ($scope.user.email !== "" && $scope.user.password !== "") {
-			$http.post("http://pickyourday.herokuapp.com/api/oauth", $scope.user).then(function successCallback(response) {
+			$http.post("https://pickyourday.herokuapp.com/api/oauth", $scope.user).then(function successCallback(response) {
 				var res = response.data;
 				if (!res.error) {
 					saveLocal("user", response.data.data);
