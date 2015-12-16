7a586c962cb25cd176ed5603100e195e273d72df
Xavier Serrano <zombispormedio007@gmail.com>
2015-12-16 20:31:06 +0100

    fdd

--- public/app/ctrl.js
+++ public/app/ctrl.js
@@ -1,7 +1,7 @@
 adminCtrl.TabCtrl = function ($rootScope, $scope, $http) {
 
 	$scope.logout = function () {
-		$http.get("http://pickyourday.herokuapp.com/api/oauth/logout").then(function successCallback(response) {
+		$http.get("https://pickyourday.herokuapp.com/api/oauth/logout").then(function successCallback(response) {
 			deleteLocal("user");
 			$rootScope.go("login");
 		}, function errorCallback(response) {
