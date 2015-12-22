c24ff3d83b98b7efad68df5c4e9224d8cb47ec8c
Xavier Serrano <zombispormedio007@gmail.com>
2015-12-22 18:01:03 +0100

    location

--- public/app/modals/location/ctrl.js
+++ public/app/modals/location/ctrl.js
@@ -3,8 +3,12 @@ adminController.LocationCtrl= function ($scope, $uibModalInstance, items) {
 
 
     $scope.location=items;
+    if(!$scope.location.name){
+        $scope.location.name="Example";
+    }
+
     $scope.ok = function () {
-        $uibModalInstance.close($scope.event);
+        $uibModalInstance.close($scope.location);
     };
 
     $scope.cancel = function () {
